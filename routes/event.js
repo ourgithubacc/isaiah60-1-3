486347c159765358c9683ddcbdecf68abd7e503f
ourgithubacc <111426399+ourgithubacc@users.noreply.github.com>
2023-01-30 21:14:42 +0100

    Add files via upload

---
+++ routes/event.js
@@ -0,0 +1,18 @@
+const express =  require('express')
+const {uploadEvent, getAllEvents, getEventByTitle, getEventsByHost, deleteEvent, updateEvent, getIperuCampusEvents, getMainCampusEvent, deleteAll, deleteAllIperuCampusEvent, deleteAllMainCampusEvent} = require('../controllers/event')
+const router = express.Router();
+const{protect, userRoleAuth} = require('../middleware/authMiddleware')
+const upload = require('../controllers/multer')
+router.route('/uploadEvent',upload.array('eventImage')).post(uploadEvent);
+router.route('/getAllEvents/:pageNo/:pageSize').get(protect,getAllEvents);
+router.route('/getEventbyTitle').get(getEventByTitle);
+router.route('/getEventsByHost').get(getEventsByHost);
+router.route('/deleteEvent').delete(deleteEvent);
+router.route('/deleteAllMainCampusEvent').delete(deleteAllMainCampusEvent)
+router.route('/deleteAllIperuCampusEvent').delete(deleteAllIperuCampusEvent)
+router.route('/deleteAll').delete(deleteAll)
+router.route('/updateEvent').put(updateEvent);
+router.route('/getIperuCampusEvents/:pageNo/:pageSize').get(protect,getIperuCampusEvents)
+router.route('/getMainCampusEvents/:pageNo/:pageSize').get(protect,getMainCampusEvent)
+
+module.exports = router;
